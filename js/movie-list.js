f489faff269c892d16fc3d1f03a4b3b234f6d949
Amberside <47558882+Amberside@users.noreply.github.com>
2025-03-31 11:53:59 +1100

    Update movie-list.js
    Planning the movieList methods.

---
+++ js/movie-list.js
@@ -0,0 +1,27 @@
+// The MovieList Class
+// This class has 2 attributes and some methods
+// Look at the Readme.md file for full list of methods.
+
+class MovieList {
+  constructor(rootId, movies){
+    this.rootId = rootId; // the html id of where the list is to be shown
+    this.movieList = movies; // Thye array of movie to be displayed
+    this.refresh();
+  }
+
+
+  // Methods
+  // Generate one row of the movieList
+  // Generate All rows of the movieList
+  // Generate a movieList based on a search term
+
+  // Remove all list elements from the DOM
+  // Refresh function
+  // Add function - add a new movie
+  // Update function - update a movie
+  // Delete function - delete a movie
+
+  // sortA2Z - sorting the movieList in ascending order
+  // sortZ2a - sorting the movieList in descending order
+  // Search - search by partial title
+}
