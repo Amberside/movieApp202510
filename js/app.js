c5454f2df4c432009a0fccf9bca18a6e43757f1c
Amberside <47558882+Amberside@users.noreply.github.com>
2025-03-31 11:21:43 +1100

    JavaScript app.js planning
    Added in javascript src to index.html
    Planned out javascript for app.js

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// Create an array movies
+
+// Create an instance of the MovieList class
+// MovieList takes in the rootId and the movie array
+
+// Event functions
+// Search Click
+// A to Z Click
+// Z to A Click
+
+// CRUD Function
+// C - Create - add new content
+// R - Read - read content
+// U - Update - update content
+// D - Delete - detele Content
+
+// Add a new movie to the list (create)
+// Update a movie in the list (update)
+// Delete a movie in the lsit (delete)
+
+// UI JavaScript
+// Tabs section
+// Function openForm
+// Take in 2 parameters, an event and an action
+// Return nothing.
+
+// Open tab by default.
+
+// Footer code
+// Get the date and inject it into the span located in the footer.
